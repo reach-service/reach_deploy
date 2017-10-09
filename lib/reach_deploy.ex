ccabe04cfcca00c5b4845eaef9f5d295d89f8e42
Hilder Santos <hilder@hildersantos.com>
2017-10-09 10:31:40 -0300

    MIx Docker on dependency

--- lib/reach_deploy.ex
+++ lib/reach_deploy.ex
@@ -1,18 +1,17 @@
 defmodule ReachDeploy do
   @moduledoc """
-  Documentation for ReachDeploy.
+  This application is intended to ease the deploy process to Docker Swarm clusters of applications
+  built with Elixir.
   """
 
-  @doc """
-  Hello world.
+  # Requires Logger for logging deploy process
+  require Logger
 
-  ## Examples
+  # TODO: Create (or read) docker compose
+  # TODO: Run Mix Docker build/release
+  # TODO: Create new docker-compose file based on configs from deploy.conf
+  # TODO: Run Mix shipit
+  # TODO: Deploy app to cluster
+  # TODO: Do cleanups
 
-      iex> ReachDeploy.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
